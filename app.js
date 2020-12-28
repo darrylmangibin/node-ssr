efc8604d332c52c085e4b68c3fd73ecc2f4df164
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-29 00:12:11 +0800

    Add controller

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
