57e49608b47a2f1703318cb504e8f33770293aa6
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-28 23:58:06 +0800

    Add ejs template engine

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+	res.status(404).render('404', {
+    pageTitle: 'Page not found'
+  })
 });
 
 const server = http.createServer(app);
