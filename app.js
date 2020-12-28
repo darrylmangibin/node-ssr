443ef593a4da53fec10a409369b54822d184de4c
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-29 00:14:19 +0800

    Add 404 controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
@@ -17,11 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', {
-    pageTitle: 'Page not found'
-  })
-});
+app.use(errorController.get404);
 
 const server = http.createServer(app);
 
