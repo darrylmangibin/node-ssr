c565284775d3d43b66eed9a98ce37cb4dd850380
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-28 23:00:12 +0800

    initial express installation

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
